2d45eb0d66b6d8e1749325f45e2068438c05d656
Steve Ripberger <sripberger@batterii.com>
2019-06-18 12:41:49 -0400

    Fix package name validator

--- internal/utils.js
+++ internal/utils.js
@@ -1,5 +1,5 @@
 exports.validatePackageName = function(name) {
-	return (/^[a-zA-Z0-9]+$/).test(name) ||
+	return (/^[a-zA-Z0-9-]+$/).test(name) ||
 		'Package name must be dash-separated lower case.';
 };
 
